1bdff7f1ef8dd08d0e815c0daaee01db0d496802
Valeria-Vogel <80950542+Valeria-Vogel@users.noreply.github.com>
2021-05-20 17:40:22 +0200

    P2_4

--- Aufgabe2_4/script/script.js
+++ Aufgabe2_4/script/script.js
@@ -6,102 +6,6 @@ var Aufgabe2_4;
     function createObj() {
         wahl = JSON.parse(Aufgabe2_4.auswahlJSON);
     }
-    let personAuswahl = {};
-    let storedSelection = localStorage.getItem("selection");
-    if (storedSelection) {
-        personAuswahl = JSON.parse(storedSelection);
-    }
-    /*  let currentParts: Beschreibung[] = [];
-      let currentPart: string = "";
-      switch (document.title) {
-          case "Alien Generator - Head":
-              currentPart = "Body";
-              currentParts = wahl.body_g;
-              break;
-          case "Alien Generator - Torso":
-              currentPart = "Hair";
-              currentParts = wahl.hair;
-              break;
-          case "Alien Generator - Legs":
-              currentPart = "Outfit";
-              currentParts = wahl.outfit;
-              break;
-          default:
-              break;
-      }
-      showPossibilities(currentParts);
-      function bodyDiv(_auswahl: Beschreibung): HTMLDivElement {
-          let div: HTMLDivElement = document.createElement("div");
-          div.classList.add("teil");
-          let image: HTMLImageElement = document.createElement("img");
-          image.src = _auswahl.bild;
-          div.appendChild(image);
-          let span: HTMLSpanElement = document.createElement("span");
-          span.innerText = _auswahl.was;
-          div.appendChild(span);
-          let button: HTMLButtonElement = document.createElement("button");
-          button.innerText = "Wählen";
-          button.addEventListener("click", wahlmoeglichkeit);
-          div.appendChild(button);
-          return div;
-          function wahlmoeglichkeit(_event: Event): void {
-              switch (currentPart) {
-                  case "body_g":
-                      personAuswahl.body_g = _auswahl;
-                      break;
-                  case "hair":
-                      personAuswahl.hair = _auswahl;
-                      break;
-                  case "outfit":
-                      personAuswahl.outfit = _auswahl;
-                      break;
-              }
-              localStorage.setItem("selection", JSON.stringify(personAuswahl));
-              switch (currentPart) {
-                  case "body_g":
-                      window.location.assign("./index.html");
-                      break;
-                  case "hair":
-                      window.location.assign("./hair.html");
-                      break;
-                  case "outfit":
-                      window.location.assign("./outfit.html");
-                      break;
-              }
-          }
-      }
-      function showPossibilities(_parts: Beschreibung[]): void {
-          let wrapper: HTMLDivElement = <HTMLDivElement>document.getElementById("auswahlBereich");
-          for (let i: number = 0; i < _parts.length; i++) {
-              let div: HTMLDivElement = bodyDiv(_parts[i]);
-              wrapper.appendChild(div);
-          }
-      }*/
-    // Aufgabe 2
-    let selectionPreview = document.getElementById("schonAusgewaehlt");
-    if (personAuswahl.body) {
-        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.body.bild)));
-    }
-    else {
-        selectionPreview.appendChild(createImage("./img/sex.png"));
-    }
-    if (personAuswahl.hair) {
-        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.hair.bild)));
-    }
-    else {
-        selectionPreview.appendChild(createImage("./img/woman-hair.png"));
-    }
-    if (personAuswahl.outfit) {
-        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.outfit.bild)));
-    }
-    else {
-        selectionPreview.appendChild(createImage("./img/male-clothes.png"));
-    }
-    function createImage(_src) {
-        let img = document.createElement("img");
-        img.src = _src;
-        return img;
-    }
     function bodyDiv(_auswahl) {
         let div = document.createElement("div");
         div.classList.add("teil");
@@ -179,5 +83,101 @@ var Aufgabe2_4;
         }
     }
     anzeige(wahl);
+    let personAuswahl = {};
+    let storedSelection = localStorage.getItem("selection");
+    if (storedSelection) {
+        personAuswahl = JSON.parse(storedSelection);
+    }
+    let currentParts = [];
+    let currentPart = "";
+    switch (document.title) {
+        case "Geschlecht":
+            currentPart = "Body";
+            currentParts = wahl.body;
+            break;
+        case "Alien Generator - Torso":
+            currentPart = "Hair";
+            currentParts = wahl.hair;
+            break;
+        case "Alien Generator - Legs":
+            currentPart = "Outfit";
+            currentParts = wahl.outfit;
+            break;
+        default:
+            break;
+    }
+    showPossibilities(currentParts);
+    function bodyDiv1(_auswahl) {
+        let div = document.createElement("div");
+        div.classList.add("teil");
+        let image = document.createElement("img");
+        image.src = _auswahl.bild;
+        div.appendChild(image);
+        let span = document.createElement("span");
+        span.innerText = _auswahl.was;
+        div.appendChild(span);
+        let button = document.createElement("button");
+        button.innerText = "Wählen";
+        button.addEventListener("click", wahlmoeglichkeit);
+        div.appendChild(button);
+        return div;
+        function wahlmoeglichkeit(_event) {
+            switch (currentPart) {
+                case "body_g":
+                    personAuswahl.body = _auswahl;
+                    break;
+                case "hair":
+                    personAuswahl.hair = _auswahl;
+                    break;
+                case "outfit":
+                    personAuswahl.outfit = _auswahl;
+                    break;
+            }
+            localStorage.setItem("selection", JSON.stringify(personAuswahl));
+            switch (currentPart) {
+                case "body":
+                    window.location.assign("./index.html");
+                    break;
+                case "hair":
+                    window.location.assign("./hair.html");
+                    break;
+                case "outfit":
+                    window.location.assign("./outfit.html");
+                    break;
+            }
+        }
+    }
+    function showPossibilities(_parts) {
+        let wrapper = document.getElementById("auswahlBereich");
+        for (let i = 0; i < _parts.length; i++) {
+            let div = bodyDiv1(_parts[i]);
+            wrapper.appendChild(div);
+        }
+    }
+    // Aufgabe 2
+    let selectionPreview = document.getElementById("schonAusgewaehlt");
+    if (personAuswahl.body) {
+        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.body.bild)));
+    }
+    else {
+        selectionPreview.appendChild(createImage("./img/sex.png"));
+    }
+    if (personAuswahl.hair) {
+        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.hair.bild)));
+    }
+    else {
+        selectionPreview.appendChild(createImage("./img/woman-hair.png"));
+    }
+    if (personAuswahl.outfit) {
+        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.outfit.bild)));
+    }
+    else {
+        selectionPreview.appendChild(createImage("./img/male-clothes.png"));
+    }
+    function createImage(_src) {
+        let img = document.createElement("img");
+        img.src = _src;
+        return img;
+    }
 })(Aufgabe2_4 || (Aufgabe2_4 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe2_4/script/script.ts
+++ Aufgabe2_4/script/script.ts
@@ -5,106 +5,6 @@ namespace Aufgabe2_4 {
         wahl = JSON.parse(auswahlJSON);
     }
 
-    let personAuswahl: Selection = {};
-    let storedSelection: string = localStorage.getItem("selection");
-    if (storedSelection) {
-        personAuswahl = JSON.parse(storedSelection);
-    }
-
-
-    /*  let currentParts: Beschreibung[] = [];
-      let currentPart: string = "";
-      switch (document.title) {
-          case "Alien Generator - Head":
-              currentPart = "Body";
-              currentParts = wahl.body_g;
-              break;
-          case "Alien Generator - Torso":
-              currentPart = "Hair";
-              currentParts = wahl.hair;
-              break;
-          case "Alien Generator - Legs":
-              currentPart = "Outfit";
-              currentParts = wahl.outfit;
-              break;
-          default:
-              break;
-      }
-      showPossibilities(currentParts);
-      function bodyDiv(_auswahl: Beschreibung): HTMLDivElement {
-          let div: HTMLDivElement = document.createElement("div");
-          div.classList.add("teil");
-          let image: HTMLImageElement = document.createElement("img");
-          image.src = _auswahl.bild;
-          div.appendChild(image);
-          let span: HTMLSpanElement = document.createElement("span");
-          span.innerText = _auswahl.was;
-          div.appendChild(span);
-          let button: HTMLButtonElement = document.createElement("button");
-          button.innerText = "Wählen";
-          button.addEventListener("click", wahlmoeglichkeit);
-          div.appendChild(button);
-          return div;
-          function wahlmoeglichkeit(_event: Event): void {
-              switch (currentPart) {
-                  case "body_g":
-                      personAuswahl.body_g = _auswahl;
-                      break;
-                  case "hair":
-                      personAuswahl.hair = _auswahl;
-                      break;
-                  case "outfit":
-                      personAuswahl.outfit = _auswahl;
-                      break;
-              }
-              localStorage.setItem("selection", JSON.stringify(personAuswahl));
-              switch (currentPart) {
-                  case "body_g":
-                      window.location.assign("./index.html");
-                      break;
-                  case "hair":
-                      window.location.assign("./hair.html");
-                      break;
-                  case "outfit":
-                      window.location.assign("./outfit.html");
-                      break;
-              }
-          }
-      }
-      function showPossibilities(_parts: Beschreibung[]): void {
-          let wrapper: HTMLDivElement = <HTMLDivElement>document.getElementById("auswahlBereich");
-          for (let i: number = 0; i < _parts.length; i++) {
-              let div: HTMLDivElement = bodyDiv(_parts[i]);
-              wrapper.appendChild(div);
-          }
-      }*/
-
-
-    // Aufgabe 2
-
-    let selectionPreview: HTMLElement = <HTMLElement>document.getElementById("schonAusgewaehlt");
-
-    if (personAuswahl.body) {
-        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.body.bild)));
-    } else {
-        selectionPreview.appendChild(createImage("./img/sex.png"));
-    }
-    if (personAuswahl.hair) {
-        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.hair.bild)));
-    } else {
-        selectionPreview.appendChild(createImage("./img/woman-hair.png"));
-    }
-    if (personAuswahl.outfit) {
-        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.outfit.bild)));
-    } else {
-        selectionPreview.appendChild(createImage("./img/male-clothes.png"));
-    }
-
-    function createImage(_src: string): HTMLImageElement {
-        let img: HTMLImageElement = document.createElement("img");
-        img.src = _src;
-        return img;
-    }
 
 
 
@@ -204,4 +104,104 @@ namespace Aufgabe2_4 {
     }
 
     anzeige(wahl);
+    let personAuswahl: Selection = {};
+    let storedSelection: string = localStorage.getItem("selection");
+    if (storedSelection) {
+        personAuswahl = JSON.parse(storedSelection);
+    }
+
+
+    let currentParts: Beschreibung[] = [];
+    let currentPart: string = "";
+    switch (document.title) {
+        case "Geschlecht":
+            currentPart = "Body";
+            currentParts = wahl.body;
+            break;
+        case "Alien Generator - Torso":
+            currentPart = "Hair";
+            currentParts = wahl.hair;
+            break;
+        case "Alien Generator - Legs":
+            currentPart = "Outfit";
+            currentParts = wahl.outfit;
+            break;
+        default:
+            break;
+    }
+    showPossibilities(currentParts);
+    function bodyDiv1(_auswahl: Beschreibung): HTMLDivElement {
+        let div: HTMLDivElement = document.createElement("div");
+        div.classList.add("teil");
+        let image: HTMLImageElement = document.createElement("img");
+        image.src = _auswahl.bild;
+        div.appendChild(image);
+        let span: HTMLSpanElement = document.createElement("span");
+        span.innerText = _auswahl.was;
+        div.appendChild(span);
+        let button: HTMLButtonElement = document.createElement("button");
+        button.innerText = "Wählen";
+        button.addEventListener("click", wahlmoeglichkeit);
+        div.appendChild(button);
+        return div;
+        function wahlmoeglichkeit(_event: Event): void {
+            switch (currentPart) {
+                case "body_g":
+                    personAuswahl.body = _auswahl;
+                    break;
+                case "hair":
+                    personAuswahl.hair = _auswahl;
+                    break;
+                case "outfit":
+                    personAuswahl.outfit = _auswahl;
+                    break;
+            }
+            localStorage.setItem("selection", JSON.stringify(personAuswahl));
+            switch (currentPart) {
+                case "body":
+                    window.location.assign("./index.html");
+                    break;
+                case "hair":
+                    window.location.assign("./hair.html");
+                    break;
+                case "outfit":
+                    window.location.assign("./outfit.html");
+                    break;
+            }
+        }
+    }
+    function showPossibilities(_parts: Beschreibung[]): void {
+        let wrapper: HTMLDivElement = <HTMLDivElement>document.getElementById("auswahlBereich");
+        for (let i: number = 0; i < _parts.length; i++) {
+            let div: HTMLDivElement = bodyDiv1(_parts[i]);
+            wrapper.appendChild(div);
+        }
+    }
+
+
+    // Aufgabe 2
+
+    let selectionPreview: HTMLElement = <HTMLElement>document.getElementById("schonAusgewaehlt");
+
+    if (personAuswahl.body) {
+        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.body.bild)));
+    } else {
+        selectionPreview.appendChild(createImage("./img/sex.png"));
+    }
+    if (personAuswahl.hair) {
+        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.hair.bild)));
+    } else {
+        selectionPreview.appendChild(createImage("./img/woman-hair.png"));
+    }
+    if (personAuswahl.outfit) {
+        selectionPreview.appendChild(createImage(localStorage.getItem(personAuswahl.outfit.bild)));
+    } else {
+        selectionPreview.appendChild(createImage("./img/male-clothes.png"));
+    }
+
+    function createImage(_src: string): HTMLImageElement {
+        let img: HTMLImageElement = document.createElement("img");
+        img.src = _src;
+        return img;
+    }
 }

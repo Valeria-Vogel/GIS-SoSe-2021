32341055dc24a3ee4ff5bfd04c2e81599dc7bdf2
Valeria-Vogel <80950542+Valeria-Vogel@users.noreply.github.com>
2021-05-30 13:24:08 +0200

    P3_1

--- Aufgabe3_1/script.js
+++ Aufgabe3_1/script.js
@@ -1,38 +1,39 @@
 "use strict";
 var Aufgabe3_1;
 (function (Aufgabe3_1) {
-    /* document.getElementById("Button").addEventListener("click", handelButtonClick);
- 
-     function handelButtonClick(): void {
-         response("https://gissose2021heroku.herokuapp.com");
-         console.log("Es läuft!");
-     }
-     async function response(_url: RequestInfo): Promise<void> {
-         let formData: FormData = new FormData(document.forms[0]);
-         let query: URLSearchParams = new URLSearchParams(<any>formData);
-         _url = _url + "?" + query.toString();
-         let serverResponse: Response = await fetch(_url);
-         let responseString: string = await serverResponse.text();
-         sessionStorage.setItem("answer", responseString);
- 
-     }
-     console.log(sessionStorage.getItem("answer"));
- */
-    let submitBtn = document.getElementById("Button");
-    submitBtn.addEventListener("click", click);
-    async function click() {
+    document.getElementById("Button").addEventListener("click", handelButtonClick);
+    function handelButtonClick() {
+        response("https://gissose2021heroku.herokuapp.com");
+        console.log("Es läuft!");
+    }
+    async function response(_url) {
         let formData = new FormData(document.forms[0]);
-        let url = "https://gissose2021heroku.herokuapp.com/";
+        // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
+        _url = _url + "?" + query.toString();
+        let serverResponse = await fetch(_url);
+        let responseString = await serverResponse.text();
+        sessionStorage.setItem("answer", responseString);
+    }
+    console.log(sessionStorage.getItem("answer"));
+    /* let submitBtn: HTMLButtonElement = <HTMLButtonElement> document.getElementById("Button");
+    submitBtn.addEventListener("click", click);
+    
+    async function click(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "https://gissose2021heroku.herokuapp.com/";
+        // tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
         url += "?" + query.toString();
-        let response = await fetch(url, { method: "get" });
-        let responseText = await response.text();
+        let response: Response = await fetch(url, { method: "get"});
+        let responseText: string = await response.text();
         showResponse(responseText);
         console.log(responseText);
     }
-    function showResponse(response) {
-        let responseDiv = document.getElementById("response");
+
+    function showResponse(response: string): void {
+        let responseDiv: HTMLDivElement = <HTMLDivElement> document.getElementById("response");
         responseDiv.innerHTML = "Serverantwort: " + response;
-    }
+    }*/
 })(Aufgabe3_1 || (Aufgabe3_1 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe3_1/script.ts
+++ Aufgabe3_1/script.ts
@@ -1,5 +1,5 @@
 namespace Aufgabe3_1 {
-   /* document.getElementById("Button").addEventListener("click", handelButtonClick);
+    document.getElementById("Button").addEventListener("click", handelButtonClick);
 
     function handelButtonClick(): void {
         response("https://gissose2021heroku.herokuapp.com");
@@ -7,6 +7,7 @@ namespace Aufgabe3_1 {
     }
     async function response(_url: RequestInfo): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
+        // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         _url = _url + "?" + query.toString();
         let serverResponse: Response = await fetch(_url);
@@ -15,13 +16,14 @@ namespace Aufgabe3_1 {
 
     }
     console.log(sessionStorage.getItem("answer"));
-*/
-    let submitBtn: HTMLButtonElement = <HTMLButtonElement> document.getElementById("Button");
+
+    /* let submitBtn: HTMLButtonElement = <HTMLButtonElement> document.getElementById("Button");
     submitBtn.addEventListener("click", click);
     
     async function click(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
         let url: string = "https://gissose2021heroku.herokuapp.com/";
+        // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url += "?" + query.toString();  
         let response: Response = await fetch(url, { method: "get"});
@@ -33,5 +35,5 @@ namespace Aufgabe3_1 {
     function showResponse(response: string): void {
         let responseDiv: HTMLDivElement = <HTMLDivElement> document.getElementById("response");
         responseDiv.innerHTML = "Serverantwort: " + response;
-    }
+    }*/
 }

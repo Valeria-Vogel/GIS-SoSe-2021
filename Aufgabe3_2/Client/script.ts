26b9a3dfdbe3d2cdc264248e97e4ce0b75a6464c
Valeria-Vogel <80950542+Valeria-Vogel@users.noreply.github.com>
2021-06-06 16:00:42 +0200

    P3_2

--- Aufgabe3_2/Client/script.ts
+++ Aufgabe3_2/Client/script.ts
@@ -1,16 +1,21 @@
 namespace Aufgabe3_1 {
 
+    let url: string = "index.html";
+
     let btn: HTMLButtonElement = <HTMLButtonElement> document.getElementById("submit");
     btn.addEventListener("click", betaetigen);
     
     async function betaetigen(): Promise<void> {
+
         let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://gissose2021heroku.herokuapp.com/";
+
+       // let url: string = "https://gissose2021heroku.herokuapp.com/";
+
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url += "?" + query.toString();  
-        let response: Response = await fetch(url, { method: "get"});
+
+        let response: Response = await fetch(url += "?" + query.toString());
         let responseText: string = await response.text();
-        console.log(responseText);     
+        alert(responseText);     
     }
 }
--- Aufgabe3_2/Server/server.ts
+++ Aufgabe3_2/Server/server.ts
@@ -21,6 +21,17 @@ export namespace Aufgabe3_1 {
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("I hear voices!"); 
+
+        if (_request.url) {
+            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+            for (let key in url.query) {
+                console.log(key + ": " + url.query[key] );
+            
+            }  
+            let jsonString: string = JSON.stringify(url.query);
+            _response.write(jsonString);
+        }
+
         _response.setHeader("content-type", "text/html; charset=utf-8");    
         _response.setHeader("Access-Control-Allow-Origin", "*");    
         _response.write(_request.url); 

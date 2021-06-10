a7c9dff57d1f41a063ea0299eea854f670c22d3d
Valeria-Vogel <80950542+Valeria-Vogel@users.noreply.github.com>
2021-06-10 17:14:01 +0200

    P3_2

--- Aufgabe3_2/Server/server.js
+++ Aufgabe3_2/Server/server.js
@@ -18,19 +18,29 @@ var Aufgabe3_1;
     }
     function handleRequest(_request, _response) {
         console.log("I hear voices!");
-        _response.setHeader("content-type", "application/json");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         if (_request.url) {
             let url = Url.parse(_request.url, true);
-            if (_request.url) {
-                let jsonString = JSON.stringify(url.query);
-                _response.write(jsonString);
+            let path = url.pathname;
+            if (path == "/html") {
+                _response.setHeader("content-type", "text/html; charset=utf-8");
+                for (let key in url.query) {
+                    _response.write("<p>" + key + ":" + url.query[key] + "</p>");
+                }
             }
-            _response.end();
-            /* console.log( "http://" + _request.headers.host + _request.url);
-             let u: URL = new URL(_request.url, "http://" + _request.headers.host);
-             console.log(u);*/
+            if (path == "/json") {
+                _response.setHeader("content-type", "application/json");
+                let sendObject = JSON.stringify(url.query);
+                console.log(sendObject);
+                _response.write(sendObject);
+            }
         }
+        _response.end();
+        /* console.log( "http://" + _request.headers.host + _request.url);
+        let jsonString: string = JSON.stringify(url.query);
+        _response.write(jsonString);
+        let u: URL = new URL(_request.url, "http://" + _request.headers.host);
+        console.log(u);*/
     }
 })(Aufgabe3_1 = exports.Aufgabe3_1 || (exports.Aufgabe3_1 = {}));
 //# sourceMappingURL=server.js.map
--- Aufgabe3_2/Server/server.ts
+++ Aufgabe3_2/Server/server.ts
@@ -1,5 +1,6 @@
 import * as Http from "http";
 
+
 import * as Url from "url";
 
 
@@ -22,18 +23,32 @@ export namespace Aufgabe3_1 {
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("I hear voices!"); 
 
-        _response.setHeader("content-type", "application/json");    
         _response.setHeader("Access-Control-Allow-Origin", "*");    
         
         if (_request.url) {
             let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-            if (_request.url) {
-                let jsonString: string = JSON.stringify(url.query);
-                _response.write(jsonString);
+            let path: string = <string>url.pathname;
+            if (path == "/html") {
+                _response.setHeader("content-type", "text/html; charset=utf-8");
+                for (let key in url.query) {
+                    _response.write("<p>" + key + ":" + url.query[key] + "</p>");
+                }  
             }
-            _response.end();
 
-           /* console.log( "http://" + _request.headers.host + _request.url);
+            if (path == "/json") {
+                _response.setHeader("content-type", "application/json");
+                let sendObject: string = JSON.stringify(url.query);
+                console.log(sendObject);
+                _response.write(sendObject);
+                
+                }  
+            }
+
+            _response.end();
+            
+            /* console.log( "http://" + _request.headers.host + _request.url);
+            let jsonString: string = JSON.stringify(url.query);
+            _response.write(jsonString);
             let u: URL = new URL(_request.url, "http://" + _request.headers.host);
             console.log(u);*/
         }

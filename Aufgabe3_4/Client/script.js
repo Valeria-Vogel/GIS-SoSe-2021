f65b45d4b0d29cac666c8c2ed65620956b23250e
Valeria-Vogel <80950542+Valeria-Vogel@users.noreply.github.com>
2021-06-20 13:13:44 +0200

    p34

--- Aufgabe3_4/Client/script.js
+++ Aufgabe3_4/Client/script.js
@@ -1,73 +1,23 @@
 "use strict";
 var Aufgabe3_4;
 (function (Aufgabe3_4) {
-    /* let btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("submit");
-     btn.addEventListener("click", klicken);
- 
- 
-     async function klicken(): Promise<void> {
-         console.log("erfolgt");
- 
-     }
-    export interface Feedback {
-        _id: string;
-        name: string;
-        firstname: string;
-        registration: number;
-        message: string;
-    }
-
+    let btn = document.getElementById("submit");
+    btn.addEventListener("click", insert);
+    let btn2 = document.getElementById("submit");
+    btn2.addEventListener("click", findAll);
     //let serverURL: string = "http://localhost:8100";
-    let serverURL: string = "https://gissose2021heroku.herokuapp.com";
-
-    document.getElementById("submit")?.addEventListener("click", insert);
-    document.getElementById("feedback")?.addEventListener("click", findAll);
-
-
-
-    async function insert(_e: Event): Promise<void> {
-
-
-        let fd: FormData = new FormData(document.forms[0]);
-        // tslint:disable-next-line: no-any
-        let query: URLSearchParams = new URLSearchParams(<any>fd);
-        let response: Response = await fetch(serverURL + "/insert?" + query);
-        console.log(await response.json());
-
-    }
-
-    async function findAll(_e: Event): Promise<void> {
-        let response: Response = await fetch(serverURL + "/read");
-        let feedbacks: Feedback[] = await response.json();
-        let out: HTMLDivElement = <HTMLDivElement>document.getElementById("output")!;
-        out.innerHTML = "";
-
-        for (let f of feedbacks) {
-            console.log(createOneFeedbackDisplay(f));
-
-        }
-    }
-
-    function createOneFeedbackDisplay(_f: Feedback): void {
-        let feedbackDiv: HTMLDivElement = document.createElement("div");
-        feedbackDiv.classList.add("one-feedback");
-        feedbackDiv.setAttribute("_id", _f._id);
-    }*/
-    //let url: string = "http://localhost:8100/";
     let url = "https://gissose2021heroku.herokuapp.com";
-    //let url: string = "";
-    let type = "";
-    let htmlSubmit = document.getElementById("submit");
-    let jsonSubmit = document.getElementById("feedback");
-    jsonSubmit.addEventListener("click", function () {
-        type = "/json";
-        betaetigen();
-    });
-    htmlSubmit.addEventListener("click", function () {
-        type = "/html";
-        betaetigen();
-    });
-    async function betaetigen() {
+    async function insert(_e) {
+        let fd = new FormData(document.forms[0]);
+        // tslint:disable-next-line: no-any
+        let query = new URLSearchParams(fd);
+        let response = await fetch(url + "/insert?" + query);
+        console.log(await response.json());
+    }
+    let type;
+    async function findAll(_e) {
+        let out = document.getElementById("output");
+        out.innerHTML = "";
         let formData = new FormData(document.forms[0]);
         url += type;
         //tslint:disable-next-line: no-any
@@ -79,8 +29,6 @@ var Aufgabe3_4;
             let responseJSON = JSON.parse(responseText);
             console.log(responseJSON);
         }
-        url = "https://gissose2021heroku.herokuapp.com";
-        //url = "http://localhost:8100/";
     }
 })(Aufgabe3_4 || (Aufgabe3_4 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe3_4/Client/script.ts
+++ Aufgabe3_4/Client/script.ts
@@ -1,13 +1,10 @@
 namespace Aufgabe3_4 {
 
-    /* let btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("submit");
-     btn.addEventListener("click", klicken);
- 
- 
-     async function klicken(): Promise<void> {
-         console.log("erfolgt");
- 
-     }
+    let btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("submit");
+    btn.addEventListener("click", insert);
+    let btn2: HTMLButtonElement = <HTMLButtonElement>document.getElementById("submit");
+    btn2.addEventListener("click", findAll);
+
     export interface Feedback {
         _id: string;
         name: string;
@@ -17,12 +14,7 @@ namespace Aufgabe3_4 {
     }
 
     //let serverURL: string = "http://localhost:8100";
-    let serverURL: string = "https://gissose2021heroku.herokuapp.com";
-
-    document.getElementById("submit")?.addEventListener("click", insert);
-    document.getElementById("feedback")?.addEventListener("click", findAll);
-
-
+    let url: string = "https://gissose2021heroku.herokuapp.com";
 
     async function insert(_e: Event): Promise<void> {
 
@@ -30,71 +22,38 @@ namespace Aufgabe3_4 {
         let fd: FormData = new FormData(document.forms[0]);
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>fd);
-        let response: Response = await fetch(serverURL + "/insert?" + query);
+        let response: Response = await fetch(url + "/insert?" + query);
         console.log(await response.json());
 
     }
+    let type: string;
 
     async function findAll(_e: Event): Promise<void> {
-        let response: Response = await fetch(serverURL + "/read");
-        let feedbacks: Feedback[] = await response.json();
+
         let out: HTMLDivElement = <HTMLDivElement>document.getElementById("output")!;
         out.innerHTML = "";
-
-        for (let f of feedbacks) {
-            console.log(createOneFeedbackDisplay(f));
-
-        }
-    }
-
-    function createOneFeedbackDisplay(_f: Feedback): void {
-        let feedbackDiv: HTMLDivElement = document.createElement("div");
-        feedbackDiv.classList.add("one-feedback");
-        feedbackDiv.setAttribute("_id", _f._id);
-    }*/
-    
-    //let url: string = "http://localhost:8100/";
-    let url: string = "https://gissose2021heroku.herokuapp.com";
-    //let url: string = "";
-    let type: string = "";
-
-    let htmlSubmit: HTMLButtonElement = <HTMLButtonElement>document.getElementById("submit");
-    let jsonSubmit: HTMLButtonElement = <HTMLButtonElement>document.getElementById("feedback");
-
-
-
-    jsonSubmit.addEventListener("click", function (): void {
-        type = "/json";
-        betaetigen();
-    });
-
-    htmlSubmit.addEventListener("click", function (): void {
-        type = "/html";
-        betaetigen();
-    });
-
-    async function betaetigen(): Promise<void> {
-
         let formData: FormData = new FormData(document.forms[0]);
         url += type;
-        
+
         //tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
 
         let response: Response = await fetch(url += "?" + query.toString());
         let responseText: string = await response.text();
         alert(responseText); //kann auskommentiert werden
-        
+
 
 
         if (type == "/json") {
             let responseJSON: JSON = JSON.parse(responseText);
             console.log(responseJSON);
         }
-        url = "https://gissose2021heroku.herokuapp.com";
-        //url = "http://localhost:8100/";
-    }
 
+    }
 }
+
+  
+
+
 
 

0a26cc5de22f8b57afdc1d89bdb8c355ae403754
Valeria-Vogel <80950542+Valeria-Vogel@users.noreply.github.com>
2021-06-20 15:31:18 +0200

    p34

--- Aufgabe3_4/Client/script.js
+++ Aufgabe3_4/Client/script.js
@@ -7,19 +7,55 @@ var Aufgabe3_4;
     btn2.addEventListener("click", insert);
     //let serverURL: string = "http://localhost:8100";
     let url = "https://gissose2021heroku.herokuapp.com";
-    let type;
-    async function insert(_e) {
+    //let type: string;
+    /* async function insert(_e: Event): Promise<void> {
+ 
+         let formData: FormData = new FormData(document.forms[0]);
+ 
+         // tslint:disable-next-line: no-any
+         let query: URLSearchParams = new URLSearchParams(<any>formData);
+ 
+         let response: Response = await fetch(url += "?" + query.toString());
+         let responseText: string = await response.text();
+ 
+         /*  console.log(await response.json());*/
+    /* console.log(responseText);
+
+     if (type == "/send") {
+         let responseJSON: JSON = JSON.parse(responseText);
+         console.log(responseJSON);
+
+
+     }
+
+ }*/
+    async function insert(type) {
         let formData = new FormData(document.forms[0]);
-        // tslint:disable-next-line: no-any
+        //tslint:disable-next-line
         let query = new URLSearchParams(formData);
-        let response = await fetch(url += "?" + query.toString());
-        let responseText = await response.text();
-        /*  console.log(await response.json());*/
-        console.log(responseText);
-        if (type == "/send") {
-            let responseJSON = JSON.parse(responseText);
-            console.log(responseJSON);
+        if (type == 0) {
+            url += "/send" + "?" + query.toString();
+            let response = await fetch(url);
+            let responseString = await response.text();
+            console.log("Data Sent", s);
+            console.log("URL:", url);
+            document.getElementById("response").innerHTML += responseString + "\n\n";
         }
+        else if (type == 1) {
+            url += "/receive" + "?" + query.toString();
+            let response = await fetch(url);
+            let responseJSON = await response.json();
+            document.getElementById("response").innerHTML += JSON.stringify(responseJSON) + "\n\n";
+            console.log("Data Received.\nURL: " + url);
+        }
+        else {
+            url += "/del" + "?" + query.toString();
+            let response = await fetch(url);
+            let responseString = await response.text();
+            console.log("Entry " + s.matrikel + " deleted.\nURL: " + url);
+            document.getElementById("response").innerHTML += responseString + "\n\n";
+        }
+        url = "https://gissose2021heroku.herokuapp.com";
     }
 })(Aufgabe3_4 || (Aufgabe3_4 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe3_4/Client/script.ts
+++ Aufgabe3_4/Client/script.ts
@@ -8,30 +8,57 @@ namespace Aufgabe3_4 {
 
     //let serverURL: string = "http://localhost:8100";
     let url: string = "https://gissose2021heroku.herokuapp.com";
-    let type: string;
+    //let type: string;
 
-    async function insert(_e: Event): Promise<void> {
+    /* async function insert(_e: Event): Promise<void> {
+ 
+         let formData: FormData = new FormData(document.forms[0]);
+ 
+         // tslint:disable-next-line: no-any
+         let query: URLSearchParams = new URLSearchParams(<any>formData);
+ 
+         let response: Response = await fetch(url += "?" + query.toString());
+         let responseText: string = await response.text();
+ 
+         /*  console.log(await response.json());*/
+    /* console.log(responseText); 
 
+     if (type == "/send") {
+         let responseJSON: JSON = JSON.parse(responseText);
+         console.log(responseJSON);
+
+
+     }
+
+ }*/
+
+    async function insert(type: number): Promise<void> {
+       
         let formData: FormData = new FormData(document.forms[0]);
-
-        // tslint:disable-next-line: no-any
+        //tslint:disable-next-line
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-
-        let response: Response = await fetch(url += "?" + query.toString());
-        let responseText: string = await response.text();
-
-        /*  console.log(await response.json());*/
-        console.log(responseText); 
-
-        if (type == "/send") {
-            let responseJSON: JSON = JSON.parse(responseText);
-            console.log(responseJSON);
-
-
+        if (type == 0) {
+            url += "/send" + "?" + query.toString();
+            let response: Response = await fetch(url);
+            let responseString: string = await response.text();
+            console.log("Data Sent", s);
+            console.log("URL:", url);
+            document.getElementById("response").innerHTML += responseString + "\n\n";
+        } else if (type == 1) {
+            url += "/receive" + "?" + query.toString();
+            let response: Response = await fetch(url);
+            let responseJSON: Student = await response.json();
+            document.getElementById("response").innerHTML += JSON.stringify(responseJSON) + "\n\n";
+            console.log("Data Received.\nURL: " + url);
+        } else {
+            url += "/del" + "?" + query.toString();
+            let response: Response = await fetch(url);
+            let responseString: string = await response.text();
+            console.log("Entry " + s.matrikel + " deleted.\nURL: " + url);
+            document.getElementById("response").innerHTML += responseString + "\n\n";
         }
-
+        url = "https://gissose2021heroku.herokuapp.com";
     }
-
 }
 
 
